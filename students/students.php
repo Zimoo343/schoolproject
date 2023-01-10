8cdd359664b8353b9db6477565f7eecc55f00608
Zimoo343 <72419060+Zimoo343@users.noreply.github.com>
2023-01-09 21:28:39 -0600

    feat: fix

--- students/students.php
+++ students/students.php
@@ -47,13 +47,79 @@
     
     
 
-    <form action="search.php" method="GET" class="search" autocomplete="off">
+    <form action="students.php" method="GET" class="search" autocomplete="off">
         <input type="search" name="search" placeholder="Buscar"></input>
         <input type="submit" value="Buscar"></input>
         <a class = "log" href="add.php"><input type="button" value="Agregar" href="add.php"></input> </a>
         <a class="pdf" id="pdf" href="javascript:generatePDF()"><input type="button" value="Reporte"></input></a>
         <a class = "log" href="../logout.php"><input type="button" value="Salir" href="logout.php"></input></a>
     </form>
+
+    <div class="tableView">
+        <table class="student_table">
+            <thead>
+                <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Calificación</th>
+                <th>Grupo</th>
+                <th>Género</th>
+                <th>Estado</th>
+                <th>Pago</th>
+                <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                $search = strtolower($_REQUEST['search']);
+
+                $q = $search
+                    ? "SELECT * FROM students_data WHERE student_id LIKE '%$search%' OR student_firstName LIKE '%$search%' OR student_lastName LIKE '%$search%'"
+                    : "SELECT * FROM students_data";
+                $student_result = $mysqli->query($q);
+                while($row = mysqli_fetch_array($student_result)) { ?>
+                    <tr>
+                        <td><?php echo $row['student_id'] ?></td>
+                        <td><?php echo $row['student_firstName'] ?></td>
+                        <td><?php echo $row['student_lastName'] ?></td>
+                        <td><?php echo $row['student_note'] < 0 ? "Sin nota" : $row['student_note'] ?></td>
+                        <td><?php echo $row['group_code'] ?></td>
+                        <td><?php echo $row['student_genre'] ?></td>
+                        <td><?php echo $row['student_state'] ?></td>
+                        <td><?php echo $row['student_payment'] ?></td>
+                        <td>
+                            <a href="edit.php?student_id=<?php echo $row['student_id']?>"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="delete.php?student_id=<?php echo $row['student_id']?>"><i class="fa-solid fa-trash-can"></i></a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+
+    </div>
+
+    <script>
+        async function generatePDF() {
+            document.getElementById("pdf").innerHTML;
+
+            //Downloading
+            var downloading = document.getElementById("body");
+            var doc = new jsPDF('l', 'pt');
+
+            await html2canvas(downloading, {
+                //allowTaint: true,
+                //useCORS: true,
+                width: 530
+            }).then((canvas) => {
+                //Canvas (convert to PNG)
+                doc.addImage(canvas.toDataURL("image/png"), 'PNG', 50, 50, 800, 400);
+            })
+
+            doc.save("ReporteStudents.pdf");
+
+        }
+    </script>
 
     <?php 
         include "tableView.php";
